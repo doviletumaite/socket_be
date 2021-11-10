80a8ca4d126f17a56a861b94b5c72aa132d81510
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-11-10 17:05:51 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,23 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/online-users', (req, res, next) => {
+    res.send({onlineUsers})
+})
+
 const httpServer = createServer(app)
 
 const io = new Server(httpServer, {allowEIO3:true})
 
 io.on("connection", (socket) => {
     console.log(socket.id)
-       socket.on("login", ({userName}) => {
+
+    socket.on("setUserName", ({userName}) => {
+        onlineUsers.push({userName, id: socket.id})
+        socket.emit("loggedIn")
+        socket.broadcast.emit("newConnection")
+    })
+       socket.on("loggetIn", ({userName}) => {
            console.log(`hello ${userName}`)
            socket.emit("welcome", {message:"hi"})
        })
